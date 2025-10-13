3b98e5cbd06295c34aa632c424e10bcd56b1be52
Borys Savenok <horosh.narod@gmail.com>
2025-10-13 05:40:35 +0300

    node home work 2

---
+++ src/models/note.js
@@ -0,0 +1,34 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+const NoteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    tags: {
+      type: String,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+export const Note = model('Note', NoteSchema);
---
+++ src/routes/notesRoutes.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { createNote } from '../controllers/notesController.js';
+import { getNoteById } from '../controllers/notesController.js';
+import { getAllNotes } from '../controllers/notesController.js';
+import { deleteNote } from '../controllers/notesController.js';
+import { updateNote } from '../controllers/notesController.js';
+const router = Router();
+
+router.get('/notes', getAllNotes);
+
+router.get('/notes/:noteId', getNoteById);
+
+router.post('/notes', createNote);
+
+router.delete('/notes/:noteId', deleteNote);
+router.patch('/notes/:noteId', updateNote);
+export default router;
